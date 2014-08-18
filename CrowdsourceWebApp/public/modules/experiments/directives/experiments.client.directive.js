a2fb6a2f00df83ba6e9633211d538cb2cfa41237
sanchitaggarwal <sanchitagarwal108@gmail.com>
2014-08-18 23:59:23 +0530

    Add drag directive

--- CrowdsourceWebApp/public/modules/experiments/directives/experiments.client.directive.js
+++ CrowdsourceWebApp/public/modules/experiments/directives/experiments.client.directive.js
@@ -25,7 +25,6 @@ angular.module('experiments').directive('lblDraggable', function() {
             false
         );
     }
-    }
 });
 
 angular.module('experiments').directive('lblDroppable', function() {
@@ -35,5 +34,50 @@ angular.module('experiments').directive('lblDroppable', function() {
             // again we need the native object
             var el = element[0];
         }
+        el.addEventListener(
+		    'dragover',
+		    function(e) {
+		        e.dataTransfer.dropEffect = 'copy';
+		        // allows us to drop
+		        if (e.preventDefault) e.preventDefault();
+		        this.classList.add('over');
+		        return false;
+		    },
+		    false
+		);
+		el.addEventListener(
+		    'dragenter',
+		    function(e) {
+		        this.classList.add('over');
+		        return false;
+		    },
+		    false
+		);
+
+		el.addEventListener(
+		    'dragleave',
+		    function(e) {
+		        this.classList.remove('over');
+		        return false;
+		    },
+		    false
+		);
+
+		el.addEventListener(
+		    'drop',
+		    function(e) {
+		        // Stops some browsers from redirecting.
+		        if (e.stopPropagation) e.stopPropagation();
+
+		        this.classList.remove('over');
+
+		        var item = document.getElementById(e.dataTransfer.getData('Text'));
+		        this.appendChild(item);
+
+		        return false;
+		    },
+		    false
+		);
+
     }
 });
